4b275a502764c90c9896c3afaf80cd316ea2ebd9
Luis Perez <ljpm9401@gmail.com>
2024-11-20 20:25:43 -0500

    Avances

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -5,7 +5,9 @@
     .titulo-principal.color-acento-contenido(data-aos="flip-up")
       .titulo-principal__numero
         span 3
-      h1 Titulo de primer nivel
+      h1 Herramientas de gestión
+    
+    p(data-aos="fade-up" data-aos-duration="850") Son aplicaciones y sistemas tecnológicos para organizar, monitorear y optimizar el flujo de trabajo en el soporte técnico y atención al cliente en la reparación de equipos. Estas herramientas mejoran la eficiencia operativa, contribuyen a una experiencia de servicio más transparente y satisfactoria para el cliente. Entre las funcionalidades más útiles se encuentran las siguientes:
 
 </template>
 
